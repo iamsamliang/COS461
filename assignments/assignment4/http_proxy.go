95b8913f15b6b8dc6150ff4cea07071c7534ae1e
Sam Liang <darkdragn20000@gmail.com>
2023-04-07 01:30:35 -0400

    dns

--- assignments/assignment4/http_proxy.go
+++ assignments/assignment4/http_proxy.go
@@ -8,7 +8,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
@@ -16,99 +15,6 @@ import (
 	"os"
 	"strings"
 )
-
-func handler(w http.ResponseWriter, req *http.Request) {
-	// if req.Method != "GET" {
-	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	// 	return
-	// }
-
-	// // fmt.Println("Absolute URL: ", req.URL.String())
-	// // relative_url := req.URL.Path
-	// // fmt.Println("relative url: ", req.URL.Path)
-
-	// // this URL should be a relative URL and not the absolute URL
-	// new_req, err := http.NewRequest("GET", req.URL.Path, nil)
-	// if err != nil {
-	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	// 	log.Fatalf("Error creating request: ", err)
-	// 	// need to create response with 500 error then do the same thing.
-	// 	resp.Write(body) // resp.Write(to socket)
-	// 	resp.Body.Close()
-	// 	net.Close
-	// 	return
-	// }
-
-	// // new_req.URL, _ = url.Parse(req.URL.Path)
-
-	// // Set the header values in the new request
-	// for key, values := range req.Header {
-	// 	for _, value := range values {
-	// 		new_req.Header.Add(key, value)
-	// 	}
-	// }
-
-	// // new_req.Header.Add("Host", req.Host)
-	// // new_req.Header.Add("Scheme", req.URL.Scheme)
-	// // new_req.Header.Add("Proto", "HTTP/1.1")
-	// // fmt.Println("new request header: ", new_req.Header)
-
-	// new_req.URL.Scheme = req.URL.Scheme
-	// new_req.URL.Host = req.Host
-	// // new_req.Host = req.Host
-	// new_req.Close = true // new_req.Header.Set("Connection", "close")
-	// new_req.Proto = req.Proto
-
-	// // reqDump, err := httputil.DumpRequest(new_req, true)
-	// // if err != nil {
-	// // 	log.Fatal(err)
-	// // }
-
-	// // fmt.Printf("REQUEST:\n%s", string(reqDump))
-	// // fmt.Println()
-
-	// // Send our modified request to the server and receive the server response
-	// proxy_client := &http.Client{}
-	// resp, err := proxy_client.Do(new_req)
-	// if err != nil {
-	// 	// return a response with error
-	// 	// if err is EOF I still return 500 error
-	// 	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	// 	log.Fatalf("Error sending request: %s\n", err)
-	// 	return
-	// }
-	// defer resp.Body.Close()
-
-	// b := make([]byte, 0, 512)
-	// for {
-	// 	if len(b) == cap(b) {
-	// 		// Add more capacity (let append pick how much).
-	// 		b = append(b, 0)[:len(b)]
-	// 	}
-	// 	n, err := resp.Body.Read(b[len(b):cap(b)])
-	// 	b = b[:len(b)+n]
-	// 	if err != nil {
-	// 		if err == io.EOF {
-	// 			err = nil
-	// 			break
-	// 		}
-	// 	}
-	// }
-
-	// resp.Body.Read(body)
-
-	// body, _ := io.ReadAll(resp.Body)
-
-	// Set the Connection header to "close".
-	// w.Header().Set("Connection", "close")
-
-	// Send the server response to the client
-	// return the entire response\
-	// w.Write(b)
-	// resp.Write(body) // resp.Write(to socket)
-	// resp.Body.Close()
-	// net.Close
-}
 
 func handleRequest(conn net.Conn, reader *bufio.Reader) {
 	req, err := http.ReadRequest(reader)
@@ -200,7 +106,6 @@ func handleRequest(conn net.Conn, reader *bufio.Reader) {
 		// return a response with error
 		// if err is EOF I still return 500 error
 		// create response with 500 error
-		fmt.Println("proxy_client.Do error: ", err)
 		resp := &http.Response{
 			Status:     "500 Internal Server Error",
 			StatusCode: 500,
@@ -240,7 +145,8 @@ func proxy(server_port string) {
 		// accept any client connection
 		conn, err := sock.Accept()
 		if err != nil {
-			log.Fatalf("Could not connect to client - %s\n", err)
+			log.Printf("Could not connect to client - %s\n", err)
+			continue
 		}
 
 		// setup a reader to continuously read packets from the connection to a buffer so that we can print immediately
--- assignments/assignment4/http_proxy_DNS.go
+++ assignments/assignment4/http_proxy_DNS.go
@@ -1,7 +1,7 @@
 /*****************************************************************************
- * http_proxy_DNS.go                                                                 
- * Names: 
- * NetIds:
+ * http_proxy_DNS.go
+ * Names: Sam Liang
+ * NetIds: saml
  *****************************************************************************/
 
 // TODO: implement an HTTP proxy with DNS Prefetching
@@ -10,3 +10,176 @@
 // with the name http_proxy_DNS.go, thus overwriting this file, then edit it
 // to add DNS prefetching (don't forget to change the filename in the header
 // to http_proxy_DNS.go in the copy of http_proxy.go)
+
+package main
+
+import (
+	"bufio"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/html"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func fetchDNS(doc *html.Node) {
+	nodes := []*html.Node{}
+	nodes = append(nodes, doc)
+
+	for len(nodes) > 0 {
+		// get the newest node (dfs) and pop it
+		num_nodes := len(nodes)
+		node := nodes[num_nodes-1]
+		nodes = nodes[:num_nodes-1]
+
+		//
+		if node.Type == html.ElementNode && node.Data == "a" {
+			for _, attr := range node.Attr {
+				if attr.Key == "href" {
+					net.LookupHost(attr.Val)
+					break
+				}
+			}
+		}
+
+		// populate with next nodes
+		next := node.FirstChild
+		for next != nil {
+			nodes = append(nodes, next)
+			next = next.NextSibling
+		}
+	}
+}
+
+func handleRequest(conn net.Conn, reader *bufio.Reader) {
+	req, err := http.ReadRequest(reader)
+	if err != nil {
+		// return a response with error
+		// if err is EOF I still return 500 error
+		// create response with 500 error
+		resp := &http.Response{
+			Status:     "500 Internal Server Error",
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(strings.NewReader("")),
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+		}
+		resp.Write(conn)
+		resp.Body.Close()
+		conn.Close()
+		return
+	}
+
+	if req.Method != "GET" {
+		// create response with 500 error
+		resp := &http.Response{
+			Status:     "500 Internal Server Error",
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(strings.NewReader("")),
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+		}
+		resp.Write(conn)
+		resp.Body.Close()
+		conn.Close()
+		return
+	}
+
+	// new_req.URL, _ = url.Parse(req.URL.Path)
+
+	// not allowed to have RequestURI set when doing a client request.
+	// req.RequestURI = ""
+
+	// relative URL
+	// req.URL.Path = req.URL.String()[strings.Index(req.URL.String()[8:], "/")+8:]
+	// req.Close = true // new_req.Header.Set("Connection", "close")
+	req.Header.Set("Connection", "close")
+	// // new_req.Header.Add("Scheme", req.URL.Scheme)
+	// // new_req.Header.Add("Proto", "HTTP/1.1")
+
+	// new_req.Close = true // new_req.Header.Set("Connection", "close")
+	req.Proto = "HTTP/1.1"
+	req.ProtoMajor = 1
+	req.ProtoMinor = 1
+
+	tsp := &http.Transport{}
+	resp, err := tsp.RoundTrip(req)
+
+	if err != nil {
+		// return a response with error
+		// if err is EOF I still return 500 error
+		// create response with 500 error
+		resp := &http.Response{
+			Status:     "500 Internal Server Error",
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(strings.NewReader("")),
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+		}
+		resp.Write(conn)
+		resp.Body.Close()
+		conn.Close()
+		return
+	}
+
+	// DNS fetching
+	graph, err := html.Parse(resp)
+	go fetchDNS(graph)
+
+	if err != nil {
+		// return a response with error
+		// if err is EOF I still return 500 error
+		// create response with 500 error
+		resp := &http.Response{
+			Status:     "500 Internal Server Error",
+			StatusCode: 500,
+			Body:       ioutil.NopCloser(strings.NewReader("")),
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+		}
+		resp.Write(conn)
+		resp.Body.Close()
+		conn.Close()
+		return
+	}
+
+	// return the entire response to the client
+	resp.Write(conn)
+	resp.Body.Close()
+	conn.Close()
+}
+
+// TODO: implement an HTTP proxy
+func proxy(server_port string) {
+	// listen on 127.0.0.1 on port server_port for client connections
+	sock, err := net.Listen("tcp", "127.0.0.1:"+server_port)
+	if err != nil {
+		log.Fatalf("Could not setup a socket and listen on port: %s\n", err)
+	}
+	defer sock.Close()
+
+	for {
+		// accept any client connection
+		conn, err := sock.Accept()
+		if err != nil {
+			log.Printf("Could not connect to client - %s\n", err)
+			continue
+		}
+
+		// setup a reader to continuously read packets from the connection to a buffer so that we can print immediately
+		// reader w/ buffer of size RECV_BUFFER_SIZE
+		inp_stream := bufio.NewReader(conn)
+		go handleRequest(conn, inp_stream)
+	}
+}
+
+// Main parses command-line arguments and calls server function
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: ./server-go [server port]")
+	}
+	server_port := os.Args[1]
+	proxy(server_port)
+}
